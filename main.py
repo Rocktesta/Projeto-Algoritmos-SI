92d4fdbc218ee4068a01c711580410c237e30ee7
João Ebbers <122540086+Rocktesta@users.noreply.github.com>
2024-07-08 18:27:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import numpy as np
 import networkx as nx
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from pyvis.network import Network
 
-node1 = 'a'
-node2 = 'b'
-node3 = 'c'
-node4 = 'd'
+
 
 class Graph:
     def __init__(self, links):
         self.link_list = links
-
-class Node:
-    def __init__(self, value):
-        self.value = value
+   
 
 def ShortestPath (graph, start, end):   # utiliza Dijkstra
     previous = {node: None for node in graph.link_list.keys()}
@@ -40,8 +35,44 @@ def ShortestPath (graph, start, end):   # utiliza Dijkstra
                 priority.sort()
     return
         
-graph = Graph({node1: {node2: 2, node3: 1},
-               node2: {node4: 10},
-               node3: {},
-               node4: {}})
-print(ShortestPath(graph, node1, node4))
+        
+
+graph = Graph({
+    'A': {'B': 2, 'C': 1, 'D': 3},
+    'B': {'E': 5, 'F': 2},
+    'C': {'D': 4, 'G': 3},
+    'D': {'H': 6},
+    'E': {'I': 2},
+    'F': {'J': 3, 'K': 1},
+    'G': {'L': 2},
+    'H': {'M': 1},
+    'I': {'N': 4},
+    'J': {'L': 100},
+    'K': {'O': 2},
+    'L': {'P': 1},
+    'M': {'Q': 3},
+    'N': {},
+    'O': {'R': 4},
+    'P': {},
+    'Q': {},
+    'R': {'S': 5},
+    'S': {'A': 20}
+})
+
+nx_graph = nx.DiGraph()
+
+for node, links in graph.link_list.items():
+    for end_node, weight in links.items():
+        nx_graph.add_edge(node, end_node, weight=weight)
+
+net = Network(height='750px', width='100%', notebook=True)
+
+for node in graph.link_list:
+    net.add_node(node, label=node)
+
+for node, edges in graph.link_list.items():
+    for end_node, weight in edges.items():
+        net.add_edge(node, end_node, label=str(weight))
+net.show('example.html')
+
+print(ShortestPath(graph, 'A', 'Q'))
