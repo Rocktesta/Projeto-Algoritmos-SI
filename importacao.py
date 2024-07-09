c4d74fb6f18544a42ee1c3f4ddf717ecce24ee20
MarceloPMelo <161610046+MarceloPMelo@users.noreply.github.com>
2024-07-09 18:49:08 -0300

    Update importacao.py

--- importacao.py
+++ importacao.py
@@ -1,16 +1,31 @@
 import pandas as pd
-
 import sqlite3
 import pandas as pd
 
+def importacao_db():
+    
+    conn = sqlite3.connect('Graph.db')
 
-conn = sqlite3.connect('Graph.db')
+    # Escreva a consulta SQL que deseja executar
+    query = 'SELECT * FROM grafo'
 
-# Escreva a consulta SQL que deseja executar
-query = 'SELECT * FROM grafo'
+    # Carregue o resultado da consulta em um DataFrame
+    df = pd.read_sql_query(query, conn)
+    print(df)
 
-# Carregue o resultado da consulta em um DataFrame
-df = pd.read_sql_query(query, conn)
-print(df)
+    conn.close()
 
-conn.close()
+    df['origem'] = df['origem'].astype(str)
+    df['destino'] = df['destino'].astype(str)
+    graph_dict = {}
+
+    # Percorre cada linha do DataFrame e preenche o dicionário
+    for _, row in df.iterrows():
+        origem, destino, peso = row['origem'], row['destino'], row['distancia']
+        if origem not in graph_dict:
+            graph_dict[origem] = {}
+        graph_dict[origem][destino] = peso
+    print(graph_dict)
+    return graph_dict
+importacao_db()
+
