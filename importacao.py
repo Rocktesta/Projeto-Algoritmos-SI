35ca45a251f446905741ebd2c924167474bf8173
luizmiguelgr <160862979+luizmiguelgr@users.noreply.github.com>
2024-07-12 13:05:28 -0300

    Comentários adicionados importacao.py

--- importacao.py
+++ importacao.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sqlite3
-import pandas as pd
+# linha "import pandas as pd" script que transforma o banco de dados com livraria pandas (já tinha)
 
 def importacao_db():
-    
+    #conecta-se ao banco de dados
     conn = sqlite3.connect('Graph.db')
 
     # Escreva a consulta SQL que deseja executar
@@ -14,10 +14,10 @@ def importacao_db():
     
 
     conn.close()
-
+    #termina a conexão 
     df['origem'] = df['origem'].astype(str)
     df['destino'] = df['destino'].astype(str)
-    graph_dict = {}
+    graph_dict = {} # o grafo é um dicionário de dicionários
 
     # Percorre cada linha do DataFrame e preenche o dicionário
     for _, row in df.iterrows():
@@ -25,6 +25,6 @@ def importacao_db():
         if origem not in graph_dict:
             graph_dict[origem] = {}
         graph_dict[origem][destino] = peso
-    return graph_dict
+    return graph_dict #retorna o grafo completo
 importacao_db()
 
