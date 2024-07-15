c910a71070cc79026671422af087e41ca550551a
claraolivcosta <161532968+claraolivcosta@users.noreply.github.com>
2024-07-14 21:07:44 -0300

    comentários

--- load_data.py
+++ load_data.py
@@ -4,23 +4,24 @@ from scipy.io import mmread
 # path relativo
 mtx_file_path = 'projeto_algoritmos\\USAir97\\USAir97.mtx'
 
-matrix = mmread(mtx_file_path)
+matrix = mmread(mtx_file_path) # Lê a matriz do arquivo .mtx
 
 dense_matrix = matrix.toarray() if hasattr(matrix, 'toarray') else matrix
 
+# Conecta ou cria um banco de dados SQLite chamado 'Graph.db'
 conn = sqlite3.connect('Graph.db')
 cur = conn.cursor()
 
 cur.execute('''
-    CREATE TABLE IF NOT EXISTS grafo (
+    CREATE TABLE IF NOT EXISTS grafo (  # Cria a tabela 'grafo' com as colunas origem, destino e distancia se ela não existir
         origem INTEGER,
         destino INTEGER,
         distancia REAL
     )
 ''')
 
-rows, cols = dense_matrix.shape
-for i in range(rows):
+rows, cols = dense_matrix.shape # obter as dimensões da matriz
+for i in range(rows): # Itera sobre todas as células da matriz
     for j in range(cols):
         value = dense_matrix[i, j]
         if value != 0:  
